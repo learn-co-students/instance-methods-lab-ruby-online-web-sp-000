2d6d91b5df7005ec04c9e5f38a553718137d0624
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-25 17:55:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end
+  def bark
+    puts "Woof!"
+  end
+end
+ 
+class Person
+def talk
+  puts "Hello World!"
+end
+def walk
+  puts "The Person is walking"
+end
+end
