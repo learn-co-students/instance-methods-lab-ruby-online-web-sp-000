6bd9d43bfa471eea272301c8e7a214f69ca920fe
Donna Amos <donnacamos.developer@gmail.com>
2019-04-08 21:04:04 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person # class method defines an object Person 
-  def talk # instance method defines talk and puts out a phrase that causes the Person object to say 
+  def talk # instance method defines talk and puts out a phrase that causes the object, Person, to say 
     puts "Hello World!"
   end
   def walk 
