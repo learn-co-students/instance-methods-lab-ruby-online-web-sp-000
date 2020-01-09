eed276d75f44c238c137949ae1ff68c2bd3f816c
Taylor Mills <taylormills24@yahoo.com>
2020-01-09 10:53:44 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog
+end
+
+fido = Dog.new
+fido
+
+
+class Dog
+end
+
+fido = Dog.new
+fido.object_id
+
+
+class Dog
+end
+
+fido = Dog.new
+fido.methods
+
+
+class Dog
+  def bark
+    puts "Woof!"
+  end
+end
+
+fido = Dog.new
+fido.bark
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,38 @@
+class Person
+end
+
+taylor_mills = Person.new
+taylor_mills
+
+
+class Person
+end
+
+taylor_mills = Person.new
+taylor_mills.object_id
+
+
+class Person
+end
+
+taylor_mills = Person.new
+taylor_mills.methods
+
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+end
+
+taylor_mills = Person.new
+taylor_mills.talk
+
+
+class Person
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+taylor_mills.walk
